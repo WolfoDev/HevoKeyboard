c5444b9d712f8d88c30ec070927ffcc704dd870e
Wolfo <f.greco@hevolus.it>
2019-07-12 11:22:57 +0200

    First commit

---
+++ Scripts/Keyboard/SliderInputField.cs
@@ -0,0 +1,30 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License. See LICENSE in the project root for license information.
+
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+namespace HoloKeyboard
+{
+    /// <summary>
+    /// This is an input field that overrides getting deselected
+    /// </summary>
+    public class SliderInputField : InputField
+    {
+        /// <summary>
+        /// Override OnDeselect
+        /// </summary>
+        /// <param name="eventData"></param>
+        public override void OnDeselect(BaseEventData eventData)
+        {
+            // Do nothing for deselection
+        }
+        
+        //public override void OnPointerClick(PointerEventData eventData)
+        //{
+        //    base.OnPointerClick(eventData);
+        //    MoveTextEnd(false);
+        //}
+    }
+}
---
+++ Scripts/Utilities/PositionOnEnable.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.MixedReality.Toolkit.Utilities;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PositionOnEnable : MonoBehaviour
+{
+    [SerializeField]
+    private Transform transformToClone;
+    [SerializeField]
+    private bool inFrontOfCamera = false;
+    [SerializeField]
+    private float cameraDistance = 2.5f;
+
+    private void OnEnable()
+    {
+        if (transformToClone)
+        {
+            transform.position = transformToClone.position;
+            transform.rotation = transformToClone.rotation;
+        }
+        else if (inFrontOfCamera)
+        {
+            transform.position = CameraCache.Main.transform.position + CameraCache.Main.transform.forward * cameraDistance;
+            transform.rotation = Quaternion.LookRotation(CameraCache.Main.transform.forward);
+        }
+    }
+}
